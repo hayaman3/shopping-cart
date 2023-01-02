5b2dac6279e307cec964424e93d26738f8a13e1a
hayaman3 <kevin.haya@yahoo.com>
2023-01-02 23:55:14 +0800

    Finished Product Card

--- src/components/pages/Shop.jsx
+++ src/components/pages/Shop.jsx
@@ -1,13 +1,55 @@
 import React from 'react';
+import productImage from '../../assets/products/product1.png';
 
 function Shop() {
   return (
-    <>
-      <div>@</div>
-      <div>Shop</div>
-    </>
+    <div className="shop">
+      <div className="product-card">
+        <img src={productImage} alt="product" className="contain" />
+        <div className="card-header">
+          <div className="label">available</div>
+          <div className="label">:action emoji</div>
+        </div>
+        <div className="card-main">
+          <div className="product-name">Product Name</div>
+          <div className="product-rating">&#9734</div>
+        </div>
+        <div className="card-footer">
+          <div className="product-price">$$100</div>
+        </div>
+      </div>
+      <div className="product-card">
+        <div className="card-header">
+          <div className="label">available</div>
+          <div className="label">:action emoji</div>
+        </div>
+        <div className="card-main">
+          <div className="product-name">Product Name</div>
+          <div className="product-rating">&#9734</div>
+        </div>
+        <div className="card-footer">
+          <div className="product-price">$$100</div>
+        </div>
+      </div>
+      <div className="product-card">
+        <div className="card-header">
+          <div className="label">available</div>
+          <div className="label">:action emoji</div>
+        </div>
+        <div className="card-main">
+          <div className="product-name">Product Name</div>
+          <div className="product-rating">&#9734</div>
+        </div>
+
+        <div className="card-footer">
+          <div className="product-price">$$100</div>
+        </div>
+      </div>
+    </div>
   );
 }
 
 export default Shop;
+
+// notes: @ product rating has randomizer for stars
 
