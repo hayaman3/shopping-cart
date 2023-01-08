8d30f3ab40aa7022876fd9a08c2e405f0bccda82
hayaman3 <kevin.haya@yahoo.com>
2023-01-08 23:14:16 +0800

    redesigned Home component

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import heroImage from '../../assets/images/hero.png';
+// import heroImage from '../../assets/images/hero.png';
+import heroImage from '../../assets/images/midjourney.png';
 
 function Home() {
   return (
     <div className="home">
       <div className="description">
         <div className="description-smaller">Fake Online Shop</div>
-        Lorem ipsum dolor sit amet, consectetur
+        <p>Lorem ipsum dolor sit amet, consectetur</p>
+        <a href="/shop" className="description-shop">
+          Shop
+        </a>
       </div>
       <img src={heroImage} alt="gradient" className="hero-image" />
     </div>
